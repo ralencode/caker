3f46d3ee411d93a655389bb37aa556d4caa0f60f
ralen <ralen@tutamail.com>
2025-05-12 18:57:52 +0300

    BACK-10 Database migration

--- Migrations/20250512155708_MinorCakeAttributesChange.Designer.cs
+++ Migrations/20250512155708_MinorCakeAttributesChange.Designer.cs
@@ -4,6 +4,7 @@ using System.Numerics;
 using Caker.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Caker.Migrations
 {
     [DbContext(typeof(CakerDbContext))]
-    partial class CakerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250512155708_MinorCakeAttributesChange")]
+    partial class MinorCakeAttributesChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250512155708_MinorCakeAttributesChange.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Caker.Migrations
+{
+    /// <inheritdoc />
+    public partial class MinorCakeAttributesChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ReqTime",
+                table: "Cakes",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<BigInteger>(
+                name: "Color",
+                table: "Cakes",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "ReqTime",
+                table: "Cakes",
+                type: "interval",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Color",
+                table: "Cakes",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(BigInteger),
+                oldType: "numeric",
+                oldNullable: true);
+        }
+    }
+}
