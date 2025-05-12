60ab6b1469ce1340009d20a89b1ff71b35f11fce
ralen <ralen@tutamail.com>
2025-05-12 19:03:52 +0300

    BACK-10 Database migration

---
+++ Migrations/20250512160258_CakeColorString.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Caker.Migrations
+{
+    /// <inheritdoc />
+    public partial class CakeColorString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                table: "Cakes",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(BigInteger),
+                oldType: "numeric",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<BigInteger>(
+                name: "Color",
+                table: "Cakes",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
