2a49c0bbff9c5c6b8d1fd967e05cdbcd8fc69056
Mivankir <148501350+Mivankir@users.noreply.github.com>
2025-05-19 16:36:57 +0300

    Add files via upload

---
+++ Caker.Test/OrderRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using Moq;
+using Caker.Models;
+using Caker.Repositories;
+using Caker.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Caker.Tests
+{
+    public class OrderRepositoryTests
+    {
+        [Fact]
+        public async Task GetByCustomer_ShouldReturnOrders()
+        {
+            var options = new DbContextOptionsBuilder<CakerDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            using var context = new CakerDbContext(options);
+            var repository = new OrderRepository(context);
+
+            var order = new Order { Id = 1, CustomerId = 5, CakeId = 1 };
+            await repository.Create(order);
+
+            var result = await repository.GetByCustomer(5);
+
+            Assert.Single(result);
+            Assert.Equal(1, result.First().CakeId);
+        }
+
+    }
+}
