d02c937733fe7f307ff0f0a100d39a18d8f04079
ralen <ralen@tutamail.com>
2025-05-23 14:42:30 +0300

    BACK-10 Image is not required anymore

--- Dto/ImageUpload.cs
+++ Dto/ImageUpload.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using Caker.Attributes.FileAttributes;
 
@@ -7,7 +6,6 @@ namespace Caker.Dto
     public record ImageUpload
     {
         [JsonPropertyName("image")]
-        [Required]
         [MaxFileSize(10 * 1024 * 1024)] // 10MB
         [AllowedExtensions([".jpg", ".jpeg", ".png"])]
         public IFormFile? Image { get; set; }
