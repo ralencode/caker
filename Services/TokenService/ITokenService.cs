c045d8d4365e82c7b593aae4e195991c7d72c62b
ralen <ralen@tutamail.com>
2025-05-25 17:39:30 +0300

    Correct interface signatures

--- Services/TokenService/ITokenService.cs
+++ Services/TokenService/ITokenService.cs
@@ -5,6 +5,9 @@ namespace Caker.Services.TokenService
 {
     public interface ITokenService
     {
+        Task<string> GenerateRefreshTokenAsync(User user);
+
+        Task<RefreshToken?> ValidateRefreshTokenAsync(string token);
         string GenerateAccessToken(User user);
         string GenerateRefreshToken();
         ClaimsPrincipal? GetPrincipalFromToken(string token);
