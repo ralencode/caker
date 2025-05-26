4adb845597c74a3e6ba3c3fe4bb0537dd7bd1cfe
ralen <ralen@tutamail.com>
2025-05-26 04:08:09 +0300

    BACK-10 Confectioner.DoCustom database migration

---
+++ Migrations/20250526010750_ConfectionerDoCustom.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Caker.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfectionerDoCustom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DoCustom",
+                table: "Confectioners",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DoCustom",
+                table: "Confectioners");
+        }
+    }
+}
