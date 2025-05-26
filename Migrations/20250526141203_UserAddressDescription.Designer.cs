6f6da3829e3c9b4eecff0c1a1f363f4ecdd6a67f
ralen <ralen@tutamail.com>
2025-05-26 17:13:05 +0300

    BACK-10 Database migrations for user's address and description

--- Migrations/20250526141203_UserAddressDescription.Designer.cs
+++ Migrations/20250526141203_UserAddressDescription.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Caker.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Caker.Migrations
 {
     [DbContext(typeof(CakerDbContext))]
-    partial class CakerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250526141203_UserAddressDescription")]
+    partial class UserAddressDescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
