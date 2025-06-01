0958220665d08597d3b9ee55fdb2b25825bb213a
ralen <ralen@tutamail.com>
2025-06-01 21:09:42 +0300

    BACK-10 Change Cake DTO fields naming from CamelCase to snake_case for correct multipart/form-data names

--- Dto/CakeDto.cs
+++ Dto/CakeDto.cs
@@ -3,44 +3,44 @@ using System.Text.Json.Serialization;
 namespace Caker.Dto
 {
     public record CreateRegularCakeRequest(
-        [property: JsonPropertyName("name")] string Name,
-        [property: JsonPropertyName("description")] string Description,
-        [property: JsonPropertyName("diameter")] double Diameter,
-        [property: JsonPropertyName("weight")] double Weight,
-        [property: JsonPropertyName("required_time")] int ReqTime,
-        [property: JsonPropertyName("price")] int Price
+        [property: JsonPropertyName("name")] string name,
+        [property: JsonPropertyName("description")] string description,
+        [property: JsonPropertyName("diameter")] double diameter,
+        [property: JsonPropertyName("weight")] double weight,
+        [property: JsonPropertyName("required_time")] int required_time,
+        [property: JsonPropertyName("price")] int price
     ) : ImageUpload;
 
     public record CreateCustomCakeRequest(
-        [property: JsonPropertyName("confectioner_id")] int ConfectionerId,
-        [property: JsonPropertyName("name")] string? Name,
-        [property: JsonPropertyName("description")] string Description,
-        [property: JsonPropertyName("fillings")] ICollection<string> Fillings,
-        [property: JsonPropertyName("required_time")] int ReqTime,
-        [property: JsonPropertyName("color")] string Color,
-        [property: JsonPropertyName("diameter")] double Diameter,
-        [property: JsonPropertyName("text")] string Text,
-        [property: JsonPropertyName("text_size")] double TextSize,
-        [property: JsonPropertyName("text_x")] double TextX,
-        [property: JsonPropertyName("text_y")] double TextY,
-        [property: JsonPropertyName("price")] int? Price,
-        [property: JsonPropertyName("image_scale")] double ImageScale
+        [property: JsonPropertyName("confectioner_id")] int confectioner_id,
+        [property: JsonPropertyName("name")] string? name,
+        [property: JsonPropertyName("description")] string description,
+        [property: JsonPropertyName("fillings")] ICollection<string> fillings,
+        [property: JsonPropertyName("required_time")] int required_time,
+        [property: JsonPropertyName("color")] string color,
+        [property: JsonPropertyName("diameter")] double diameter,
+        [property: JsonPropertyName("text")] string text,
+        [property: JsonPropertyName("text_size")] double text_size,
+        [property: JsonPropertyName("text_x")] double text_x,
+        [property: JsonPropertyName("text_y")] double text_y,
+        [property: JsonPropertyName("price")] int? price,
+        [property: JsonPropertyName("image_scale")] double image_scale
     ) : ImageUpload;
 
     public record UpdateCustomCakeRequest(
-        [property: JsonPropertyName("confectioner_id")] int? ConfectionerId,
-        [property: JsonPropertyName("name")] string? Name,
-        [property: JsonPropertyName("description")] string? Description,
-        [property: JsonPropertyName("fillings")] ICollection<string>? Fillings,
-        [property: JsonPropertyName("required_time")] int? ReqTime,
-        [property: JsonPropertyName("color")] string? Color,
-        [property: JsonPropertyName("diameter")] double? Diameter,
-        [property: JsonPropertyName("text")] string? Text,
-        [property: JsonPropertyName("text_size")] double? TextSize,
-        [property: JsonPropertyName("text_x")] double? TextX,
-        [property: JsonPropertyName("text_y")] double? TextY,
-        [property: JsonPropertyName("price")] int? Price,
-        [property: JsonPropertyName("image_scale")] double? ImageScale
+        [property: JsonPropertyName("confectioner_id")] int? confectioner_id,
+        [property: JsonPropertyName("name")] string? name,
+        [property: JsonPropertyName("description")] string? description,
+        [property: JsonPropertyName("fillings")] ICollection<string>? fillings,
+        [property: JsonPropertyName("required_time")] int? required_time,
+        [property: JsonPropertyName("color")] string? color,
+        [property: JsonPropertyName("diameter")] double? diameter,
+        [property: JsonPropertyName("text")] string? text,
+        [property: JsonPropertyName("text_size")] double? text_size,
+        [property: JsonPropertyName("text_x")] double? text_x,
+        [property: JsonPropertyName("text_y")] double? text_y,
+        [property: JsonPropertyName("price")] int? price,
+        [property: JsonPropertyName("image_scale")] double? image_scale
     ) : ImageUpload;
 
     public record CakeResponse(
