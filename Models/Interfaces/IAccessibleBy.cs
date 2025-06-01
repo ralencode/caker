d60def7476121f97f6e2bbeaaf841458f465e1b8
ralen <ralen@tutamail.com>
2025-06-01 23:24:16 +0300

    Make AllowedUserIds ignored by json

--- Models/Interfaces/IAccessibleBy.cs
+++ Models/Interfaces/IAccessibleBy.cs
@@ -1,7 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace Caker.Models.Interfaces
 {
     public interface IAccessibleBy
     {
+        [JsonIgnore]
+        [JsonPropertyName("allowed_user_ids")]
         ICollection<int> AllowedUserIds { get; }
     }
 }
